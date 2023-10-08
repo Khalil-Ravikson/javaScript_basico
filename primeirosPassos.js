66785d1c656b6416b2c46753df9c58daa36d7518
Khalil Ravikson Alcantara do Carmo <khalilrvk2017@gmail.com>
2023-10-08 18:20:24 -0300

    Variaveis Primitivas e algumas operações

---
+++ primeirosPassos.js
@@ -0,0 +1,53 @@
+// Variaveis primitivas
+
+// Variavel tipo Number
+const tipoNumber = 12;
+console.log(tipoNumber);
+
+// float (ponto flutuante)
+const flutuante = 0.32;
+const semZero = .5;
+console.log(semZero);
+console.log(flutuante);
+
+// Operações: Soma, divisão, subtração e multiplicação
+
+const somaNumber = tipoNumber + 12;
+const divisaoNumber = tipoNumber/2;
+const subtraçãoNumber = tipoNumber - 12;
+const multiplicaçãoNumber = tipoNumber * 3;
+console.log(somaNumber + divisaoNumber + subtraçãoNumber + multiplicaçãoNumber);
+
+// Var tipos String
+
+const tipoString = "Ola mundo";
+console.log(tipoString);
+
+// concatenação 
+const tipoString2 = '. Adeus mundo "GOOD BYE"';
+console.log(tipoString + tipoString2);
+
+//transformação/conversão do código Unicode
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+//utilização do length
+var x = "Mozilla";
+var empty = "";
+
+console.log(
+  "Mozilla possui " + x.length + " unidades de código de comprimento",
+);
+
+
+console.log("A string vazia possui um comprimento de " + empty.length);
+
+
+
