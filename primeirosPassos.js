79aec698a6c5509a53b702576a48ccf4643f6e07
Khalil Ravikson Alcantara do Carmo <khalilrvk2017@gmail.com>
2023-11-04 18:40:49 -0300

    modificações

--- primeirosPassos.js
+++ primeirosPassos.js
@@ -11,7 +11,6 @@ console.log(semZero);
 console.log(flutuante);
 
 // Operações: Soma, divisão, subtração e multiplicação
-
 const somaNumber = tipoNumber + 12;
 const divisaoNumber = tipoNumber/2;
 const subtraçãoNumber = tipoNumber - 12;
@@ -19,15 +18,14 @@ const multiplicaçãoNumber = tipoNumber * 3;
 console.log(somaNumber + divisaoNumber + subtraçãoNumber + multiplicaçãoNumber);
 
 // Var tipos String
-
 const tipoString = "Ola mundo";
 console.log(tipoString);
 
-// concatenação 
+// Concatenação 
 const tipoString2 = '. Adeus mundo "GOOD BYE"';
 console.log(tipoString + tipoString2);
 
-//transformação/conversão do código Unicode
+// Transformação/conversão do código Unicode
 const cifrao = '\u0024'
 const aMaiusculo = '\u0041'
 const tique = '\u2705'
@@ -38,15 +36,14 @@ console.log(aMaiusculo)
 console.log(tique)
 console.log(hiragana)
 
-//utilização do length
+// Utilização do length
 var x = "Mozilla";
 var empty = "";
 
+// O ".lenght" conta quantos caracteres tem uma string
 console.log(
   "Mozilla possui " + x.length + " unidades de código de comprimento",
 );
-
-
 console.log("A string vazia possui um comprimento de " + empty.length);
 
 
